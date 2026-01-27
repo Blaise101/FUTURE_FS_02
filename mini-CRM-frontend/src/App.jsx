0a64a1bb05a195de95439fd9889a30d1ad09246b
Blaise202 <izerimanablaise5@gmail.com>
2026-01-28 01:14:30 +0200

    Sidebar

--- mini-CRM-frontend/src/App.jsx
+++ mini-CRM-frontend/src/App.jsx
@@ -1,9 +1,5 @@
 import Layout from "./components/Layout";
 
 export default function App() {
-  return (
-    <div className="flex h-screen overflow-hidden bg-gray-50">
-      <Layout />
-    </div>
-  );
+  return <Layout />;
 }
--- mini-CRM-frontend/src/components/Sidebar.jsx
+++ mini-CRM-frontend/src/components/Sidebar.jsx
@@ -1,7 +1,53 @@
+import { MdOutlineDashboard } from "react-icons/md";
+import { SlPeople } from "react-icons/sl";
+import { IoSettingsOutline } from "react-icons/io5";
+import { useState } from "react";
+import Me from "../assets/me.png";
+
 export default function Sidebar() {
+  const menuBtns = [
+    { name: "Dashboard", icon: MdOutlineDashboard },
+    { name: "Leads", icon: SlPeople },
+    { name: "Settings", icon: IoSettingsOutline },
+  ];
+
+  const [activeBtn, setActiveBtn] = useState("Dashboard");
+
   return (
     <aside className="w-64 bg-white border-r border-gray-200 hidden md:flex flex-col">
-      Sidebar
+      <div className="p-8 flex">
+        <div className="w-8 h-8 bg-indigo-600 mr-2 rounded-lg flex items-center justify-center text-white font-bold">
+          A
+        </div>
+        <h1 className="text-xl font-bold text-gray-900 tracking-tight">
+          AbcCRM
+        </h1>
+      </div>
+      <div className="mt-3">
+        <div className="block px-8">
+          {menuBtns.map((menuBtn) => (
+            <button
+              onClick={() => setActiveBtn(menuBtn.name)}
+              className={` my-3 w-full flex ${activeBtn == menuBtn.name ? "bg-blue-100" : ""} items-center gap-3 px-4 py-3 text-sm font-medium rounded-lg transition-colors`}
+            >
+              <menuBtn.icon size={21} /> {menuBtn.name}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="mt-auto p-6 border-t border-gray-100">
+        <div className="flex items-center gap-3">
+          <img
+            src={Me}
+            className="w-10 h-10 rounded-full border border-gray-200"
+            alt="Profile"
+          />
+          <div>
+            <p className="text-sm font-semibold text-gray-900">Blaise Kamali</p>
+            <p className="text-xs text-gray-500">Business Owner</p>
+          </div>
+        </div>
+      </div>
     </aside>
   );
 }
